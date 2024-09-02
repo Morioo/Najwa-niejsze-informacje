1ca7ad8010a72056f97fb82a270013d257f0a3d5
Morioo <107799693+Morioo@users.noreply.github.com>
2024-09-02 16:41:00 +0200

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+
+
+
+Console.WriteLine("Hello, World!");
+
+
+public class NazwaKlasy {}
+
+
+/*Struktura
+ 
+ public struct Coord {
+
+public int x;
+public int y;
+
+}
+
+{
+public coord (int x, int y)
+{
+this.x = x;
+this.y = y;
+
+
+}
+
+enum WeekDay {
+
+(Posiadają wartości od 0 do 6)
+
+Mondey,
+tuesday,
+wednesday,
+thursday,
+friday,
+saturday,
+sunday
+
+}
+
+enum Categories{
+
+elegtronics = 1,
+food = 3,
+automotive = 5,
+arts = 14,
+fahion = 5
+
+}
+
+ Coord coord = new coord();
+coord.x = 3;
+coord.y = 4;
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+
+
+//Wartości 4 204 -34 -3332, 203.2 98.1
+// wartosci znakowe char 'd', "   char chr = 'LewicaPrawica'
+//wartosc logiczna np. bool ( true, false )
+
+//typeof logiczny  BOOL
+
+//typ całkowitoliczbowy byte od 0 do 255  byte btx = 3;
+
+//decimal ( od -7.9 * 10^28 do 7.9 * 10^28) / 10^
+
+//double (+/-) 5.0*10^-324 do (+/-) 1.7 * 10381
+
+//float = 9.4f
+
+//int całkowitoliczbowy od -2,(14)... do 2,(14)
+
+//long całkowitoliczbowy (wiekszy int)
+
+//uint (32bity) od 0 do 4,294967295
+
+//ulong dużyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy
+
+//object obj = 4;
+
+/* object obj2;
+obj2 = 100;
+
+
+
+//int i = (int)object;
+//dynamic x = 30;
+//string str = 'ddddddddddd';
+
+___________________________________________
+var kompilator sam rozpozna typ danych
+___________________________________________
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+*/
